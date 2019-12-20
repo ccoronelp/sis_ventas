a4dafb8891edb5f7e72abec7811fcf05c88d023d
jccoronelp <jocar.144@gmail.com>
2019-12-20 09:30:33 -0500

    se modificó el index y la conexión con la base de datos

--- bd_conexion/conexion.php
+++ bd_conexion/conexion.php
@@ -1,11 +1,10 @@
 <?php
 
-<<<<<<< HEAD
 class conexion{
 	static public function conectar(){
-		$cadena="mysql:host=localhost;dbname=bd_ventas";
-		$usuario="root";
-		$password="";
+		$cadena="mysql:host=localhost;dbname=admin_bd_ventas";
+		$usuario="admin_ventas";
+		$password="JsNXkfq2O2";
 		$pdo = new PDO($cadena,$usuario,$password);
 
 		$pdo->exec("set names utf8");
@@ -42,41 +41,6 @@ class ModeloUsuario
 
 		$consulta_preparada->close();
     	$consulta_preparada=null;
-=======
-class BaseDatos
-{
-	
-	static public function Conexion()
-	{
-		$cadena="mysql:host=localhost;dbname=fpdf";
-		$usuario="root";
-		$password="";
-		
-		$pdo = new PDO($cadena,$usuario,$password);
-		$pdo->exec("set name utf8");
-
-		return $pdo;
-
-	}
-}
-
-/**
- * 
- */
-class Productos 
-{
-	
-	static public function TraerProductos()
-	{
-		$sql_consulta = 'SELECT * FROM productos';
-		$consulta_preparada = BaseDatos::Conexion()->prepare($sql_consulta);
-		$consulta_preparada->execute();
-
-		return $consulta_preparada->fetchAll();	
-
-		$consulta_preparada->close();
-		$consulta_preparada = "";
->>>>>>> 572cfdc93437d830b63039e89aed16b2a3077427
 	}
 }
 
